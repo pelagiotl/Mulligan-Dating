44625db692f8a822af54a33c0e497a94e637441e
Luke <pelagiotl@gmail.com>
2026-01-05 17:46:30 -0800

    Fix import.meta.env type declarations

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,12 +1,16 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_API_URL?: string
-  readonly VITE_NGROK_URL?: string
-  [key: string]: any
+declare global {
+  interface ImportMetaEnv {
+    readonly VITE_API_URL?: string
+    readonly VITE_NGROK_URL?: string
+    [key: string]: any
+  }
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
 }
 
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
+export {}
 
